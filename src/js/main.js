fa05f893fbd069f2c5550c60afb72d765b5d66a1
Daniil Postnov <dan.postnov@gmail.com>
2018-09-27 16:28:16 +0300

    import external libraries

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,8 @@
     Javascript files including like this:
 */
 
+//import ('../../bower_components/jquery/dist/jquery.min.js');
 //import ('partails/common.js');
+//import ('../../bower_components/fancybox/dist/jquery.fancybox.min.js');
+//import ('../../bower_components/slick-carousel/slick/slick.min.js');
+
