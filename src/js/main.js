2bafd59a8d8169722c5c608ecf5a1ac0f51a6d2e
Daniil Postnov <dan.postnov@gmail.com>
2018-09-29 15:20:54 +0300

    finish main page styles

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,6 @@
 */
 
 //import ('../../bower_components/jquery/dist/jquery.min.js');
-//import ('partails/common.js');
 //import ('../../bower_components/fancybox/dist/jquery.fancybox.min.js');
 //import ('../../bower_components/slick-carousel/slick/slick.min.js');
-
+//import ('partails/common.js');
--- src/js/partails/common.js
+++ src/js/partails/common.js
@@ -1,1 +1,28 @@
-console.log('Hello guest!');
+document.addEventListener('DOMContentLoaded', function () {
+
+    console.log(19);
+
+    // Первый параметр - массив классов, высоту которых надо вычитать. Второй - селектор элемента, которому нужно устанавливать минимальную высоту
+
+    pageSetMinHieght(['.header', '.footer'], '.page__content');
+
+    function pageSetMinHieght(classesArray, selectorContent) {
+        var commonHeight = 0,
+            deviceHeight = window.innerHeight,
+            totalHeight = 0;
+
+        classesArray.forEach(function (item) {
+            var elem = document.querySelector(item);
+
+            if (elem) {
+                var height = elem.scrollHeight || 0;
+                commonHeight += height;
+            }
+        });
+
+        totalHeight = deviceHeight - commonHeight;
+
+        document.querySelector(selectorContent).style.minHeight = totalHeight + 'px';
+    }
+
+});
