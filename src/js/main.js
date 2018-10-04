c5b5ac7aac424bb46bb297eeaefbb0d11418cc8c
Daniil Postnov <dan.postnov@gmail.com>
2018-10-04 16:18:26 +0300

    add animate for pages

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,13 @@
     Javascript files including like this:
 */
 
+
+
 //import ('../../bower_components/jquery/dist/jquery.min.js');
 //import ('../../bower_components/fancybox/dist/jquery.fancybox.min.js');
 //import ('../../bower_components/slick-carousel/slick/slick.min.js');
 //import ('partails/inputmask.js');
+//import ('partails/wow.min.js');
+new WOW().init();
 //import ('partails/countUp.js');
 //import ('partails/common.js');
